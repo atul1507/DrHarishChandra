9bff547eef6ee271e0c8eb3ebe0f7cccf594f530
atul1507 <atulkumarthakur155@gmail.com>
2026-01-09 02:03:34 +0530

    Updated Footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,27 @@ function App() {
   const isHome = location.pathname === "/";
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Navbar />
 
-      <main className="pb-8 max-w-7xl mx-auto px-6 py-10">
+      {/* Main content grows to push footer down */}
+      <main className="flex-grow max-w-7xl mx-auto px-6 py-10">
         {isHome ? (
           // Home page: full width
           <AppRoutes />
         ) : (
           // Other pages: sidebar + content
           <div className="flex flex-col lg:flex-row gap-8">
-            <ProfileSidebar key={!isHome} />
+            <ProfileSidebar />
             <div className="flex-1">
               <AppRoutes />
             </div>
           </div>
         )}
       </main>
+
       <Footer />
-    </>
+    </div>
   );
 }
 
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      
+      <Navbar />
+
+      {/* This pushes footer down when content is short */}
+      <main className="flex-grow">
+        {children}
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
